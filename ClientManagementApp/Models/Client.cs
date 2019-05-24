5c5ef72915985c7a79938fdf5f919c4a00e9194f
Ben Ahmed Zohra <benahmedzohra@hotmail.com>
2019-05-25 01:43:06 +0200

    .Net Core 2.0 Simple App

---
+++ ClientManagementApp/Models/Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClientManagementApplication.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public int CP { get; set; }
+        [Required]
+        public string Address { get; set; }
+    }
+}
---
+++ ClientManagementApp/Models/ClientSearchViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientManagementApplication.Models
+{
+    public class ClientSearchViewModel
+    {
+        public List<Client> Clients { get; set; }
+        public SelectList FirstNames { get; set; }
+        public string FirstName { get; set; }
+        public string SearchString { get; set; }
+    }
+}
